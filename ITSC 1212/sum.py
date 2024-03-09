5da87b004a3f66843d032a5759e66d3620ee7af3
Jonny <giancarlopretell1@gmail.com>
2024-03-09 15:10:52 -0500

    Upate from Codingrooms

---
+++ ITSC 1212/sum.py
@@ -0,0 +1,6 @@
+# Write an program that accepts three numbers and prints their sum
+Num1 = input ("Enter your first number: " ) 
+Num2 = input("Enter your second number: ") 
+Num3 = input("Enter your third number: ")
+Sum = int(Num1)+int(Num2)+int(Num3)
+print (Sum)
