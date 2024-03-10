8be72004dccccfed9626599342bc6ca712ddc69e
Jonny <giancarlopretell1@gmail.com>
2024-03-10 14:59:33 -0400

    CodingRooms

---
+++ ITSC 1212/booleanReturn.py
@@ -0,0 +1,10 @@
+def isEven(num):
+   return num % 2 == 0
+
+
+
+
+# the following statements call the isEven function.
+# when everything works as expected the program will display True and False.
+print(isEven(56))
+print(isEven(1))
